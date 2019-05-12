be28c46f0c49c7117aa6a4354d4d84ac545d6ccc
Chechko Ivan <chechko_ivan@MacBook-Pro-Chechko.local>
2019-05-12 18:08:39 +0300

    add slider

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -48,30 +48,30 @@ $(document).ready(function() {
     });
   }
 
-  // if ($(".main_small-cards-wrapper").length) {
-  //   $(".main_small-cards-wrapper").slick({
-  //     infinite: true,
-  //     slidesToShow: 3,
-  //     slidesToScroll: 1,
-  //     arrows: false,
-  //     autoplay: true,
-  //     autoplaySpeed: 3000,
-  //     responsive: [
-  //       {
-  //         breakpoint: 768,
-  //         settings: {
-  //           slidesToShow: 2
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 576,
-  //         settings: {
-  //           slidesToShow: 1
-  //         }
-  //       }
-  //     ]
-  //   });
-  // }
+  if ($(".main_small-cards-wrapper").length) {
+    $(".main_small-cards-wrapper").slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  }
 
   $(".stepper").inputSpinner();
   $("#burger").click(toggleMenu);
